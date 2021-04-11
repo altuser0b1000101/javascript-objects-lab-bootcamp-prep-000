647934df542bda871d529079f1038f0513174ddd
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-11 21:11:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign ({}, object, {[key] : value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+const object = { key: '3' };
+ 
+function deleteFromObjectByKey(object, key){
+delete object[key];
+return object;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+delete object[key];
+return object;
+}
